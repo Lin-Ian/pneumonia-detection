67af72b513fc80e990b9524e4f5ac8db8476650d
Ian Lin <ianrp.lin@gmail.com>
2023-08-18 10:41:10 -0400

    Improve results page

--- App/app.py
+++ App/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 from keras.models import load_model
 from model import predict_image
+from PIL import Image
+import io
+import base64
+import os
 
 app = Flask(__name__)
 model = load_model('model.h5')
@@ -18,10 +22,25 @@ def upload():
     image_name = image.filename
     image.save(image_name)
 
+    # Save image to pass to HTML template
+    im = Image.open(image_name)
+    data = io.BytesIO()
+    im.save(data, "JPEG")
+    encoded_img_data = base64.b64encode(data.getvalue())
+
     # Make prediction on image
     prediction, normal_confidence, pneumonia_confidence = predict_image(model, image_name)
 
-    return f'{prediction} / {normal_confidence} / {pneumonia_confidence}'
+    # Convert decimal to percentage
+    normal_confidence = "{:.2%}".format(normal_confidence)
+    pneumonia_confidence = "{:.2%}".format(pneumonia_confidence)
+
+    # Remove image from root folder
+    os.remove(image_name)
+
+    return render_template('prediction.html',
+                           img_data=encoded_img_data.decode('utf-8'), prediction=prediction,
+                           normal_confidence=normal_confidence, pneumonia_confidence=pneumonia_confidence)
 
 
 if __name__ == "__main__":
