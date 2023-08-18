faf6770a72e3e43f5788812ea52d3be0f11ca7d2
Ian Lin <ianrp.lin@gmail.com>
2023-08-18 09:42:37 -0400

    Basic functionality to predict image using web app

--- App/app.py
+++ App/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from keras.models import load_model
+from model import predict_image
 
 app = Flask(__name__)
+model = load_model('model.h5')
 
 
 @app.route("/")
@@ -8,9 +11,17 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/upload")
+@app.route("/upload", methods=['POST', 'GET'])
 def upload():
-    return "Uploaded"
+    # Get uploaded image
+    image = request.files['image']
+    image_name = image.filename
+    image.save(image_name)
+
+    # Make prediction on image
+    prediction, normal_confidence, pneumonia_confidence = predict_image(model, image_name)
+
+    return f'{prediction} / {normal_confidence} / {pneumonia_confidence}'
 
 
 if __name__ == "__main__":
